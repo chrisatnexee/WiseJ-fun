ba4449224b6e70ed161173a771debe8c82fe66c6
Chris Catto <Chris@nexee.com>
2024-09-17 12:56:34 -0400

    init commit

---
+++ WisejWebDesktopApp1CattoFun/WisejWebDesktopApp1CattoFun/MyDesktop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Wisej.Web;
+
+namespace WisejWebDesktopApp1CattoFun
+{
+    public partial class MyDesktop : Desktop
+    {
+        public MyDesktop()
+        {
+            InitializeComponent();
+        }
+    }
+}
